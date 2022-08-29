d2b10a0a07292f3bc86bac3635ef0f3fc6aabcd6
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2022-08-29 15:47:28 +0530

    Create Linked_List.cpp
    Added main function and initialized the structure

---
+++ Linked_List.cpp
@@ -0,0 +1,32 @@
+//Menu driven C++ Program to perform operations on a linked list
+//Code by Aadith Sukumar (https://github.com/aadi1011)
+
+#include <iostream>
+using namespace std;
+
+// Creating structure for node declaration
+struct node
+{
+	int data;
+	struct node* link;
+};
+
+//main function
+int main()
+{
+	struct node* head = NULL;
+	struct node* second = NULL;
+	struct node* third = NULL;
+	
+	head = (struct node*) malloc(sizeof(struct node));
+	second = (struct node*)malloc(sizeof(struct node));
+	third = (struct node*)malloc(sizeof(struct node));
+	
+	head->data = 45;
+	head->link = second;
+	second->data = 98;
+	second->link = third;
+	third->data = 31;
+	third->link = NULL;
+	menu(head);
+}
