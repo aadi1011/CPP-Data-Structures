77a2b95cdbbf394692480e24ce6571a8a50f4a08
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2022-11-05 23:27:58 +0530

    Update circular_linked_list.cpp

--- circular_linked_list.cpp
+++ circular_linked_list.cpp
@@ -32,20 +32,21 @@ void insert_beg(struct node* head)
     
 	struct node* newnode = NULL;
     newnode = (struct node*)malloc(sizeof(struct node*));
-    newnode->data = n;	
+    newnode->data = n;
+    
 	struct node* temp = head->link;
 	while (temp->link != head)
-	{	
-		temp = temp->link;
-	}
+    {
+        temp = temp->link;
+    }
 	temp->link = newnode;
 	newnode->link = head;
 	head=newnode;
 
-	cout<<endl;
+    cout<<endl;
 	traverse(head);
 }
-	
+
 //Insert at a position in the circular list
 void pos_insert(struct node* head)
 {
@@ -70,7 +71,7 @@ void pos_insert(struct node* head)
 	temp->link = newnode;
 	cout<<endl;
 	traverse(head);
-}    
+}
 
 // Deleting from the beginning of the circular list
 void delete_beg(struct node* head)
@@ -96,10 +97,10 @@ void menu(struct node* head)
 	cout<<"2. Insert at the beginning of the list"<<endl;
 	cout<<"3. Insert at a position in the list"<<endl;
 	cout<<"4. Delete from the beginning of the list"<<endl;
-	cout<<"5. Delete from a position in the list"<<endl;
-	cout<<"6. Exit"<<endl;
+	cout<<"5. Exit"<<endl;
 	cout<<"Enter choice: ";
 	cin>>choice;
+
 	if(choice==1)
 	{
 		traverse(head);
@@ -118,20 +119,6 @@ void menu(struct node* head)
 	}
 	else if(choice==5)
 	{
-		// cout<<"Enter position to delete: ";
-		// int position;
-		// cin>>position;
-		// struct node* temp = head;
-		// for (int i = 1; i < position - 1; i++)
-		// {
-		// 	temp = temp->link;
-		// }
-		// temp->link = temp->link->link;
-		// cout<<endl;
-		// traverse(head);
-	}
-	else if(choice==6)
-	{
 		exit(0);
 	}
 	else
@@ -141,7 +128,6 @@ void menu(struct node* head)
 	menu(head);
 	
 }
-
 
 //Main function
 int main()
@@ -161,6 +147,6 @@ int main()
     third->data=300;
     third->link=head;
     cout<<endl;
-    menu(head);
+	menu(head);
     return 0;
 }
