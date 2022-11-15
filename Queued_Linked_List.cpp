0495f6f4d4540126dc5705a8a23469123bf4cb1b
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2022-11-15 21:28:41 +0530

    Update Queued_Linked_List.cpp

--- Queued_Linked_List.cpp
+++ Queued_Linked_List.cpp
@@ -11,3 +11,27 @@ struct node
     struct node* link;
 };  
 struct node *top = (struct node*) malloc(sizeof(struct node));
+
+//Push function at the end
+void push(int n)
+{
+    struct node* newnode = NULL;
+    newnode = (struct node*) malloc(sizeof(struct node));
+    if (!newnode)
+    {
+        cout<<"\nOverflow condition";
+    }
+    else
+    {
+        newnode -> data = n;
+        newnode -> link = NULL;
+        struct node* temp = top;
+        while(temp != NULL)
+        {
+            temp=temp->link;
+        }
+        temp->data=n;
+        temp->link=newnode;
+    }
+    display();
+}
