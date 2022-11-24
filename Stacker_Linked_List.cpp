57d3d2226e6d6a47566832df3e7e8de0e84958d9
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2022-11-24 23:13:38 +0530

    Create Stacker_Linked_List.cpp

---
+++ Stacker_Linked_List.cpp
@@ -0,0 +1,12 @@
+// C++ code for Linked list with stack Push, Pop, Display operation
+// By Aadith Sukumar (https://github.com/aadi1011)
+
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node* link;
+};  
+node*top;
