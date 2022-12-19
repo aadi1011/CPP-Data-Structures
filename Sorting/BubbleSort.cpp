2fe0a47003643c29fde0a36565a7e1576bb4e4ae
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2022-12-19 18:34:04 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void bubblesort(int array1[],int n) {
+	cout << "\nBubble Sorting by each pass: \n";
+	for (int j = 0; j < n-1; j++)
+	{
+		int flag = 0;		//flag for optimization
+		for (int k = 0; k < n-1; k++)
+		{
+			if (array1[k] > array1[k + 1]) 
+			{
+				int temp = array1[k];
+				array1[k] = array1[k + 1];
+				array1[k + 1] = temp;
+			}
+			for (int i = 0; i < n; i++)
+			{
+				cout << array1[i] << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	cout << "Sorted Array: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << array1[i] << " ";
+	}
+}
 int main() 
 {
 	int array1[100]{}, n;
