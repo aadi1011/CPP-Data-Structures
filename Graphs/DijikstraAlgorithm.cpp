5ec60f115a4a36ab117a7ef827417706e244312e
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-03-04 23:54:26 +0530

    Update DijikstraAlgorithm.cpp

--- Graphs/DijikstraAlgorithm.cpp
+++ Graphs/DijikstraAlgorithm.cpp
@@ -7,3 +7,10 @@ using namespace std;
 
 // Number of vertices in the graph
 #define V 9
+
+// Creating minimum_key() for finding the vertex that has minimum key-value and that is not added in MST yet
+int minimum_key(int k[], bool mst[])
+{
+    int minimum = INT_MAX;
+    int min, i;
+
