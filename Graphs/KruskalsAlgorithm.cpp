557ed562ef15d9334c55293b3bc417f8b076973a
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-03-17 19:58:05 +0530

    Create

---
+++ Graphs/KruskalsAlgorithm.cpp
@@ -0,0 +1,8 @@
+// C++ program to implement Kruskal's algorithm
+// Code by Aadith Sukumar (https://www.github.com/aadi1011)
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+# define vertices 5
+# include <limits.h>
