eb8b5cdf0e62b665807788f49302a43146c300f2
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-03-18 23:28:49 +0530

    Update KruskalsAlgorithm.cpp

--- Graphs/KruskalsAlgorithm.cpp
+++ Graphs/KruskalsAlgorithm.cpp
@@ -25,3 +25,18 @@ int minimum_key(int k[], int mst[])
     return min;
 }
 
+// Create kruskal() method for constructing and printing the MST
+// The g[vertices][vertices] is an adjacency matrix that defines the graph for MST
+void kruskal(int g[vertices][vertices])
+{
+    // Create an array of size equal to total number of vertices for storing the MST  
+    int parent[vertices];
+    // Create k[vertices] array for selecting an edge having minimum weight
+    int k[vertices];
+    int mst[vertices];
+    int i, count, edge, v; //V is the vertex
+    for (i=0; i<vertices;i++)
+    {
+        k[i] = INT_MAX;
+        mst[i] = 0;
+    }
