0bd10cacdd8c6959ff7f2a8ed24027f63c8db7ac
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-03-18 23:28:36 +0530

    Update KruskalsAlgorithm.cpp

--- Graphs/KruskalsAlgorithm.cpp
+++ Graphs/KruskalsAlgorithm.cpp
@@ -6,3 +6,22 @@
 using namespace std;
 # define vertices 5
 # include <limits.h>
+
+// Function to find the vertex with minimum key value, from the set of vertices not yet included in MST
+int minimum_key(int k[], int mst[])
+{
+    int minimum = INT_MAX;
+    int min, i;
+
+    // Iterate over all vertices to find the vertex with minimum key-value
+    for (i = 0; i < vertices; i++)
+    {
+        if(mst[i]==0 && k[i]<minimum)
+        {
+            minimum = k[i];
+            min = i;
+        }
+    }
+    return min;
+}
+
