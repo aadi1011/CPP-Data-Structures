fe32d4bf3f6229b314e675d172b0887c21fb0b9d
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-03-21 19:05:29 +0530

    Update PrimsAlgorithm.cpp

--- Graphs/PrimsAlgorithm.cpp
+++ Graphs/PrimsAlgorithm.cpp
@@ -24,3 +24,19 @@ int minimum_key(int k[], int mst[])
     }
     return min;
 }
+
+// Create prim() method for constructing and printing the MST
+// The g[vertices][vertices] is an adjacency matrix that defines the graph for MST
+void prim(int g[vertices][vertices])
+{
+    // Create an array of size equal to total number of vertices for storing the MST  
+    int parent[vertices];
+    // Create k[vertices] array for selecting an edge having minimum weight
+    int k[vertices];
+    int mst[vertices]; //mst is an array that stores the vertices that are included in MST
+    int i, count, edge, v; //V is the vertex, count is the number of edges to be included in MST, edge is the selected edge
+    for (i=0; i<vertices;i++)
+    {
+        k[i] = INT_MAX;
+        mst[i] = 0;
+    }
