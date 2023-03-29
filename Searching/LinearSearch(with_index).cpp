ccec6da136493424fbc3ec687061cab36a649a80
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-03-29 23:22:29 +0530

    Update LinearSearch(with_index).cpp

--- Searching/LinearSearch(with_index).cpp
+++ Searching/LinearSearch(with_index).cpp
@@ -1,3 +1,8 @@
 #include<iostream>
 using namespace std;
 int main()
+{
+    int arr[100]{}, total, i, num, arrTemp[50]{}, j = 0, chk = 0;
+    cout << "Enter the Size for Array: ";
+    cin >> total;
+    cout << "Enter " << total << " Elements: " << endl;
