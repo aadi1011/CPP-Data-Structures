a7937f3d3374ce7958e53d1a4236bf9d0317735e
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-04-03 22:42:29 +0530

    Create BinarySearch.cpp

---
+++ Searching/BinarySearch.cpp
@@ -0,0 +1,13 @@
+// Menu driven Binary Search using Iteration and Recursion
+#include <iostream>
+using namespace std;
+
+int binarySearchIter(int arr[], int n, int num)
+{
+	int low = 0, high = n - 1;
+	while (low <= high){
+		int mid = (low + high) / 2;
+		if (num == arr[mid]){
+			cout << "Value is at index: " << mid;
+			break;
+		}
