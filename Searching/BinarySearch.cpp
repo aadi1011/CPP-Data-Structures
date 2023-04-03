993d169afdcd6f8ff95148beaf2b6b38f2651a90
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-04-03 22:45:42 +0530

    Update BinarySearch.cpp

--- Searching/BinarySearch.cpp
+++ Searching/BinarySearch.cpp
@@ -11,3 +11,12 @@ int binarySearchIter(int arr[], int n, int num)
 			cout << "Value is at index: " << mid;
 			break;
 		}
+		else if (num < arr[mid]){
+			high = mid - 1;
+		}
+		else{
+			low = mid + 1;
+		}
+	}
+	return -1;
+}
